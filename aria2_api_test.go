820f7108b766340f4c1f65e0226e27e050e31893
nicolov <nicolov@users.noreply.github.com>
2017-09-28 09:25:29 -0700

    improve testing

--- aria2_api_test.go
+++ aria2_api_test.go
@@ -14,14 +14,14 @@ import (
 
 const endpointUrl = "http://localhost:6800/jsonrpc"
 
-func TestMain(m *testing.M) {
-	errs := make(chan int, 1)
-
+// Wrap aria2 daemon setup/teardown code
+func testWithAriaDaemon(t *testing.T, f func(*testing.T)) {
 	ariaCmd := exec.Command("aria2c",
 		"--enable-rpc",
 		"--rpc-listen-all")
 
-	// Kill aria even if the golang code panics
+	// Set up a process group so that aria is killed even if the golang
+	// code panics.
 	ariaCmd.SysProcAttr = &syscall.SysProcAttr{
 		Pdeathsig: syscall.SIGTERM,
 	}
@@ -40,7 +40,7 @@ func TestMain(m *testing.M) {
 			break
 		}
 
-		time.Sleep(200 * time.Millisecond)
+		time.Sleep(50 * time.Millisecond)
 		retries += 1
 	}
 
@@ -49,63 +49,63 @@ func TestMain(m *testing.M) {
 		fmt.Fprint(w, "Hello world")
 	})
 	go func() {
-		err := http.ListenAndServe(":7575", nil)
-
-		if err != nil {
-			errs <- -1
-		}
+		http.ListenAndServe(":7575", nil)
 	}()
 
-	// test
-	go func() {
-		errs <- m.Run()
-	}()
+	// Actually run the test now
+	f(t)
 
-	res := <-errs
-
-	os.Exit(res)
+	// Tear down
+	ariaCmd.Process.Kill()
 }
 
 // Test that we can successfully download a single file
 func TestDownload(t *testing.T) {
-	require := require.New(t)
+	testWithAriaDaemon(t, func(t *testing.T) {
+		require := require.New(t)
 
-	const sampleUri = "http://localhost:7575/test_file"
+		const sampleUri = "http://localhost:7575/test_file"
 
-	client := NewAriaClient(endpointUrl)
+		client := NewAriaClient(endpointUrl)
 
-	// No download in the queue
-	stats, _ := client.GetGlobalStat()
-	require.Equal(stats.NumActive, "0")
-	require.Equal(stats.NumStopped, "0")
+		// No download in the queue
+		stats, err := client.GetGlobalStat()
+		require.NoError(err)
+		require.Equal(stats.NumActive, "0")
+		require.Equal(stats.NumStopped, "0")
 
-	// Add an URI
-	downloadId, err := client.AddUri(sampleUri)
-	require.NoError(err)
-	require.NotEqual("", downloadId)
+		// Add an URI
+		downloadId, err := client.AddUri(sampleUri)
+		require.NoError(err)
+		require.NotEqual("", downloadId)
 
-	time.Sleep(20 * time.Millisecond)
+		time.Sleep(20 * time.Millisecond)
 
-	stats, _ = client.GetGlobalStat()
-	require.Equal(stats.NumActive, "0")
-	require.Equal(stats.NumStopped, "1")
+		stats, _ = client.GetGlobalStat()
+		require.Equal(stats.NumActive, "0")
+		require.Equal(stats.NumStopped, "1")
 
-	downloadStatus, err := client.TellStatus(downloadId)
-	require.NoError(err)
-	require.Equal("complete", downloadStatus.Status)
+		downloadStatus, err := client.TellStatus(downloadId)
+		require.NoError(err)
+		require.Equal("complete", downloadStatus.Status)
 
-	require.Equal(len(downloadStatus.Files), 1)
-	downloadPath := downloadStatus.Files[0].Path
-	_, err = os.Stat(downloadPath)
-	require.False(os.IsNotExist(err), "Downloaded file not found")
+		require.Equal(len(downloadStatus.Files), 1)
+		downloadPath := downloadStatus.Files[0].Path
+		_, err = os.Stat(downloadPath)
+		require.False(os.IsNotExist(err), "Downloaded file not found")
 
-	// Clean up
-	_ = os.Remove(downloadPath)
+		// Clean up
+		_ = os.Remove(downloadPath)
+	})
 }
 
-// Test queue
-func TestQueue(t *testing.T) {
-	//r := require.New(t)
-	//client := NewAriaClient(endpointUrl)
-
-}
+//// Test queue
+//func TestQueue(t *testing.T) {
+//	r := require.New(t)
+//	client := NewAriaClient(endpointUrl)
+//
+//	// No download in the queue
+//	stats, _ := client.GetGlobalStat()
+//	r.Equal(stats.NumActive, "0")
+//	r.Equal(stats.NumStopped, "0")
+//}
